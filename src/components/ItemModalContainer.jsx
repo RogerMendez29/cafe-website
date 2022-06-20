3a861bfadc79bdd29c5297a53937338d9502d550
Roger Mendez <roger61087@gmail.com>
2022-06-20 15:53:17 -0400

    Updated

---
+++ src/components/ItemModalContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ItemModalContainer = ({item}) => {
+
+  console.log(item);
+
+  return (
+    <div className="fixed top-[12rem]  w-full md:w-[40rem] h-[40rem] bg-gray-100 drop-shadow-md flex flex-col z-[101] ">
+      <div> hey</div>
+    </div>
+  );
+};
+
+export default ItemModalContainer;
